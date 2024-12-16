0431de6db20178c9668a1ddab7c2ef26e07b00e6
Giougt <154607600+Giougt@users.noreply.github.com>
2024-12-16 23:59:11 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,11 @@
 import datetime
+import random
+
+points = 0
+#earn points random for login 
+def EarnPoint(points):
+    points = random(10)
+    return points
 
 def log():
     nowTime = datetime.datetime.now().strftime("%H:%M:%S")
@@ -6,17 +13,10 @@ def log():
     strLog = f"Date: {nowDate} Time: {nowTime}\n"
     with open('login_history.txt', 'a') as f:
         f.write(strLog)
-
 try:
     with open('login_history.txt', 'x') as file:
         print("file create")
 except FileExistsError:
     print("File already exists.")
     log()
-
-#earn points random for login 
-def EarnPoint(time):
-    if (time?):
-        points = points + 10
-    ...
-    return points
+    EarnPoint()
